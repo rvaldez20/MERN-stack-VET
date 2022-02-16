f6680829aced2664b0ece070efb41b8d0ef548c7
rvaldez20 <rvaldez20@gmail.com>
2022-02-15 22:44:35 -0600

    [ADD] Se crea la configuración basica del servidor con express.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hola");
+import express from "express";
+
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("Hola Mundo desde Express");
+});
+
+app.listen(4000, () => {
+  console.log("Server listen on port 4000");
+});

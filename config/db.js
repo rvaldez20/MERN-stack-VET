8c2ea9d325c03cf051abb937c6fe52b291c3ac0a
rvaldez20 <rvaldez20@gmail.com>
2022-02-16 22:05:40 -0600

    [ADD] Se agrega la configuración de las variables de entorno para la uri de conexión a la DB

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const conectarDB = async () => {
   try {
-    const db = await mongoose.connect(process.env.conectionDB, {
+    const db = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       // useUnifieldTopology: true,
     });
--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
 import conectarDB from "./config/db.js";
 
 const app = express();
 
+// Configuración de variables de entorno
+dotenv.config();
+
+// Conexion a la base de datos
 conectarDB();
 
 app.use("/", (req, res) => {
   res.send("Hola Mundo!!");
 });
 
+// Listener del servidor de express
 app.listen(4000, () => {
   console.log("Server listen on port 4000");
 });

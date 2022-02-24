1363b5857d1b634a252f416838ab2d8b36877f3b
rvaldez20 <rvaldez20@gmail.com>
2022-02-23 22:23:38 -0600

    [UPD] Se modifican los controladores para que retornen formatos JSON

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -1,9 +1,9 @@
 const registrar = (req, res) => {
-  res.send("Desde API/VETERINARIOS");
+  res.json({ msg: "Registrando Usuario" });
 };
 
 const perfil = (req, res) => {
-  res.send("Desde API/VETERINARIOS/PERFIL");
+  res.json({ msg: "Mostrando Perfil" });
 };
 
 export { registrar, perfil };
--- routes/veterinarioRoutes.js
+++ routes/veterinarioRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { registrar, perfil } from "../controllers/veterinarioController.js";
 const router = express.Router();
 
-router.get("/", registrar);
+router.post("/", registrar);
 router.get("/perfil", perfil);
 
 export default router;

6a2f0e98e4cd9f4edc7a95c21af0f95da3b132e8
rvaldez20 <rvaldez20@gmail.com>
2022-03-12 14:08:35 -0600

    [ADD] Se implementa la validación para evitar usuarios (email) repetidos

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -2,7 +2,14 @@ import Veterinario from "../models/Veterinario.js";
 
 const registrar = async (req, res) => {
   // Obtenemos los datos del formulario
-  // const { email, password, nombre } = req.body;
+  const { email } = req.body;
+
+  // validar si el ususrio esta registrado
+  const existeUsuario = await Veterinario.findOne({ email });
+  if (existeUsuario) {
+    const error = new Error("Usuario ya registrado");
+    return res.status(400).json({ msg: error.message });
+  }
 
   try {
     // Guardar un nuevo Veterinario

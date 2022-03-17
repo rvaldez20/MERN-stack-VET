e97f0ebaeed13efa33cecf1954a3ace0c48a2697
rvaldez20 <rvaldez20@gmail.com>
2022-03-17 13:18:08 -0600

    [ADD] Se valida que se esta recibiendo el token
    En la petición se envia en el header como BearerToken
    Se valida que viene el token(JWT) y que sea Bearer

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,15 @@
 const checkAuth = (req, res, next) => {
-  console.log("Desde mi Middleware");
+  // console.log(req.headers.authorization.split(" "));
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    console.log("Si tiene el token con Bearer");
+  }
+
+  const error = new Error("Token no Válido o inexistente");
+  res.status(403).json({ msg: error.message });
+
   next();
 };
 

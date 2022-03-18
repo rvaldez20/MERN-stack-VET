12f484f10c189ce1a917d92a77c8e4216ff16841
rvaldez20 <rvaldez20@gmail.com>
2022-03-18 14:33:01 -0600

    [ADD] Se obtiene y separa el token (sin el bearer) y se verifica
    Con esto se obtiene el id del usurio y se guarada en el request (req.veterinario) y se inicia la sesión

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,14 +1,37 @@
-const checkAuth = (req, res, next) => {
+import jwt from "jsonwebtoken";
+import Veterinario from "../models/Veterinario.js";
+
+const checkAuth = async (req, res, next) => {
+  let token;
   // console.log(req.headers.authorization.split(" "));
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
-    console.log("Si tiene el token con Bearer");
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // console.log(decoded);
+
+      // buscamos al usuario que viene el jwt y se asigna le req para crear una sesion
+      req.veterinario = await Veterinario.findById(decoded.id).select(
+        "-password -token -confirmado"
+      );
+      // console.log(veterinario);
+      return next();
+    } catch (error) {
+      // console.log(error)
+      const e = new Error("Token no Válido");
+      return res.status(403).json({ msg: e.message });
+    }
   }
 
-  const error = new Error("Token no Válido o inexistente");
-  res.status(403).json({ msg: error.message });
+  // validamos si existe un token
+  if (!token) {
+    const error = new Error("Token no Válido o inexistente");
+    res.status(403).json({ msg: error.message });
+  }
 
   next();
 };
